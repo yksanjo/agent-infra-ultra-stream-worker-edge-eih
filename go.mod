24c005ba63d6f0a60311dd08efa9de488833df9d
Yoshi Kondo <yksanjo@gmail.com>
2026-02-22 15:14:59 -0500

    chore: add baseline repo standards and CI

--- go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/yksanjo/ultra-stream-worker-edge-eih
-
-go 1.21
-
-require (
-	github.com/gin-gonic/gin v1.9.1
-)
--- main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestHealth(t *testing.T) {
-	if true != true {
-		t.Error("Expected true")
-	}
-}
